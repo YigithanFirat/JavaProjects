ad8e245b71039f5fd2ea934ee3f20ad24a516906
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2024-10-20 19:38:56 +0300

    Update YüzeKadarOlanSayılarınToplamıForDöngüsü.java
    • Hesaplama hataları giderildi.
    • 100'ün de dahil edilmesi sağlandı.

--- YüzeKadarOlanSayılarınToplamıForDöngüsü.java
+++ YüzeKadarOlanSayılarınToplamıForDöngüsü.java
@@ -2,13 +2,15 @@ package Abusivesnake;
 
 public class YüzeKadarOlanSayılarınToplamıForDöngüsü 
 {
-	public static void main(String[] args)
+	
+	public static void main(String[] args) 
 	{
+		
 		int toplam = 0;
-		for(int i = 0; i < 100; i++)
+		for(int i = 0; i <= 100; i++)
 		{
-			toplam = toplam + i;
+			toplam += i; // toplam = toplam + i;
 		}
-		System.out.println("Toplam Sonucu: " + toplam);
+		System.out.println("Toplam = " + toplam);
 	}
 }
