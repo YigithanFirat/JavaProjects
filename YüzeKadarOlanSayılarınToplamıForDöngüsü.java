f6b662e22a172e82bca359ee65558eafe9534d5d
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2024-10-20 19:01:13 +0300

    Create YüzeKadarSayılarıForDöngüsüyleToplamınıHesaplama.java
    • Toplam değişkeni tanımlandı.
    • For döngüsüyle "i" sayacı her işlemden sonra +1 olacak şekilde arttırıldı.
    • "i" sayacı toplam değişkeninin yeni değeriyle sürekli toplandı.
    • Veri çıkışı ekrana verildi.

---
+++ YüzeKadarOlanSayılarınToplamıForDöngüsü.java
@@ -0,0 +1,14 @@
+package Abusivesnake;
+
+public class YüzeKadarOlanSayılarınToplamıForDöngüsü 
+{
+	public static void main(String[] args)
+	{
+		int toplam = 0;
+		for(int i = 0; i < 100; i++)
+		{
+			toplam = toplam + i;
+		}
+		System.out.println("Toplam Sonucu: " + toplam);
+	}
+}
