9613fd4d7b46c571018d45feebacaebe5cbf857b
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2024-10-23 16:36:06 +0300

    Create SayıTahminOyunu.java
    Scanner kütüphanesi eklendi.
    Random kütüphanesi eklendi.
    sayac = 0, tahmin ve rastgele_sayi olmak üzere değğişkenler belirlendi.
    rastgele_sayi sistem tarafından 1-50 arasında alındı.
    Kullanıcıya bir tahminlik hak verildi.
    Çıkan sonucu göre while() döngüsü koyuldu ve  eşit değilse döngü içindeki durumlar gerçekleşecek.
    tahmin küçükse bilgi verip tekrar tahmin yaptırılacak ve sayaç arttırılacak.
    tahmin büyükse bilgi verip tahmin isteyecek ve sayaç arttırılacak.
    Eğer ki bilinirrse while'dan çıkıp kaç seferde bilindiği bilgisi verilecek.

---
+++ SayıTahminOyunu.java
@@ -0,0 +1,35 @@
+package Abusivesnake;
+import java.util.Scanner;
+import java.util.Random;
+public class SayıTahminOyunu 
+{
+	public static void main(String[] args)
+	{
+		int sayac = 0, tahmin, rastgele_sayi;
+		Random rast = new Random();
+		Scanner scan = new Scanner(System.in);
+		rastgele_sayi = 1 + rast.nextInt(49);
+		System.out.print("Sayıyı tahmin ediniz: ");
+		tahmin = scan.nextInt();
+		sayac++;
+		while(rastgele_sayi != tahmin)
+		{
+			if(tahmin < rastgele_sayi)
+			{
+				System.out.println("Tahmininiz çok düşük, daha yüksek bir sayı tahmininde bulununuz!");
+				System.out.print("Sayıyı tahmin ediniz: ");
+				tahmin = scan.nextInt();
+				sayac++;
+			}
+			else
+			{
+				System.out.println("Tahmininiz çok yüksek, daha yüksek bir sayı tahmininde bulununuz!");
+				System.out.print("Sayıyı tahmin ediniz: ");
+				tahmin = scan.nextInt();
+				sayac++;
+			}
+		}
+		System.out.println(sayac + " seferde sayıyı bildiniz!");
+		scan.close();
+	}
+}
