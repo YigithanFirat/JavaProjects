f433f2c09fe82c19471f71ceaa24f8890f25e749
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2024-10-23 16:20:33 +0300

    Create ÇarpmaİşleminiToplamaİşlemiyleYapmak.java
    Scanner kütüphanesi eklendi.
    toplam = 0, sayi1, sayi2 değişkenleri tanımlandı.
    Scanner nesnesi oluşturuldu.
    sayi1 ve sayi2 değerleri kullanıcıdan alındı.
    for döngüsüyle sayi2 kadar dönmesi sağlandı.
    For her döndüğünde toplamın üstüne sayi1'le toplaması böylelikle sayi2 kadar sayi1'i toplaması sağlandı. Böylelikle sayi2*sayi1 çarpımıı elde edildi.
    Sonuç ekrana çarpım şeklinde yazdırıldı.

---
+++ ÇarpmaİşleminiToplamaİşlemiyleYapmak.java
@@ -0,0 +1,19 @@
+package Abusivesnake;
+import java.util.Scanner;
+public class ÇarpmaİşleminiToplamaİşlemiyleYapmak 
+{
+	public static void main(String[] args) 
+	{
+		int toplam = 0, sayi1, sayi2;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Birinci sayıyı giriniz: ");
+		sayi1 = scan.nextInt();
+		System.out.print("İkinci sayıyı giriniz: ");
+		sayi2  = scan.nextInt();
+		for(int i = 0; i < sayi2; i++)
+		{
+			toplam += sayi1;
+		}
+		System.out.println(sayi1 + "X" + sayi2 + "= " + toplam);
+	}
+}
