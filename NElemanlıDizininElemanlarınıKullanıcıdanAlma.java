c36c9b8170aadc28dd756952d30e043a9f18b8b8
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2024-10-24 18:20:21 +0300

    Create  NElemanlıDizininElemanlarınıKullanıcıdanAlma.java
    Scanner kütüphanesi eklendi.
    Scanner nesnesi türetildi.
    dizi_kac_elemanli değişkeni tanımlandı.
    Dizinin eleman sayısı kullanıcıdan alındı.
    Dizi tanımlandı.
    for döngüsüyle dizinin elemanları kullanıcıdan alındı.
    Scanner kapatıldı.

---
+++ NElemanlıDizininElemanlarınıKullanıcıdanAlma.java
@@ -0,0 +1,19 @@
+package Abusivesnake;
+import java.util.Scanner;
+public class NElemanlıDizininElemanlarınıKullanıcıdanAlma 
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		int dizi_kac_elemanli;
+		System.out.print("Dizinin kaç elemanlı olmasını istersiniz? ");
+		dizi_kac_elemanli = scan.nextInt();
+		int [] dizi = new int[dizi_kac_elemanli];
+		for(int i = 0; i < dizi.length; i++)
+		{
+			System.out.print("Dizinin " + (i+1) + " elemanını giriniz: ");
+			dizi[i] = scan.nextInt();
+		}
+		scan.close();
+	}
+}
