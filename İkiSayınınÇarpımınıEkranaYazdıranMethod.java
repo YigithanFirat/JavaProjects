f7b039e4a26a140f21dd09a7abce9e3c27ee935e
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2024-10-25 19:30:27 +0300

    Create İkiSayınınÇarpımınıEkranaYazzdıranMethod.java
    Scanner kütüphanesi eklendi.
    Main methodunda sayi1, sayi2 adında double değişkenler tanımmlandı.
    Birinci ve ikinci sayı kullanıcıdan alındı.
    Scanner kapatıldı.
    Carpim_Yazdir() methodu çağrılııp sayılar methoda atandı.
    Carpim_Yazdir methodu tanımlandı.

---
+++ İkiSayınınÇarpımınıEkranaYazdıranMethod.java
@@ -0,0 +1,20 @@
+package Abusivesnake;
+import java.util.Scanner;
+public class İkiSayınınÇarpımınıEkranaYazdıranMethod 
+{
+	public static void Carpim_Yazdir(double a, double b)
+	{
+		System.out.println(a*b);
+	}
+	public static void main(String[] args)
+	{
+		double sayi1, sayi2;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Birinci sayıyı giriniz: ");
+		sayi1 = scan.nextInt();
+		System.out.print("İkinci sayıyı giriniz: ");
+		sayi2 = scan.nextInt();
+		scan.close();
+		Carpim_Yazdir(sayi1, sayi2);
+	}
+}
