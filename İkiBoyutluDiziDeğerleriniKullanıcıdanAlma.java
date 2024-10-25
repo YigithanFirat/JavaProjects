bffff7565b125316e837b96c6b69470dfd1fbc2f
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2024-10-25 17:52:16 +0300

    Create İkiBoyutluDiziDeğerleriniKullanıcıdanAlma.java
    Scanner kütüphanesi eklendi.
    matris1, matris2 değişkenleri tanımlandı.
    Scanner nesnesi türetildi.
    matris1 ve matris2 kullanıcıdan alınarak bir dizi oluşturuldu.
    İç içe 2 for döngüsüyle dizinin elemanları alındı.
    İç içe 2 for  döngüsüyle dizi elemanları teker teker ekrana yazıldı.

---
+++ İkiBoyutluDiziDeğerleriniKullanıcıdanAlma.java
@@ -0,0 +1,32 @@
+package Abusivesnake;
+import java.util.Scanner;
+public class İkiBoyutluDiziDeğerleriniKullanıcıdanAlma 
+{
+	public static void main(String[] args)
+	{
+		int matris1, matris2;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Dizinin birinci matrisini giriniz: ");
+		matris1 = scan.nextInt();
+		System.out.print("Dizinin ikinci matrisini giriniz: ");
+		matris2 = scan.nextInt();
+		int [][] dizi = new int[matris1][matris2];
+		for(int i = 0; i < dizi.length; i++)
+		{
+			for(int j = 0; j < dizi.length; j++)
+			{
+				System.out.print("Bir sayı giriniz: ");
+				dizi[i][j] = scan.nextInt();
+			}
+		}
+		for(int i = 0; i < dizi.length; i++)
+		{
+			for(int j = 0; j < dizi.length; j++)
+			{
+				System.out.println(dizi[i][j] + " ");
+			}
+			System.out.println("\n");
+		}
+		scan.close();
+	}
+}
