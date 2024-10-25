fe4382c6fbccad73e4c0cf9f442c89008590ef66
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2024-10-25 18:15:32 +0300

    Create NxMBoyutluDiziOluşturma.java
    Scanner kütüphanesi eklendi.
    N ve  M  değişkenleri tanımlandı.
    N = satır, M = sütun olacak şekilde kullanıcıdan alındı.
    Dizi oluşturuldu.
    İç içe for döngüsüyle dizinin sayısal değerleri kullanıcıdan alındı.
    İkinci iç içe for döngüsüyle dizi elemanları tab boşluğu olacak şekilde satır şeklinde ve sütün olacak şekilde yazıldı.

---
+++ NxMBoyutluDiziOluşturma.java
@@ -0,0 +1,31 @@
+package Abusivesnake;
+import java.util.Scanner;
+public class NxMBoyutluDiziOluşturma 
+{
+	public static void main(String[] args)
+	{
+		int N, M;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Dizinin satır sayısını giriniz: ");
+		N = scan.nextInt();
+		System.out.print("Dizinin sütun sayısını giriniz: ");
+		M = scan.nextInt();
+		int [][] dizi = new int[N][M];
+		for(int i = 0; i < N; i++)
+		{
+			for(int j = 0; j < M; j++)
+			{
+				System.out.print("Bir sayı giriniz: ");
+				dizi[i][j] = scan.nextInt();
+			}
+		}
+		for(int i = 0; i < N; i++)
+		{
+			for(int j = 0; j < M; j++)
+			{
+				System.out.print(dizi[i][j] + "\t");
+			}
+			System.out.println("\n");
+		}
+	}
+}
